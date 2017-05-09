4c4d7d8beea610380b8b580cc71e75307975646b
Charles Stuart <charles.stuart@bodybuilding.com>
2017-05-09 16:39:45 -0600

    Build dist files

--- dist/truncate.js
+++ dist/truncate.js
@@ -1,4 +1,12 @@
-(function (module, $, undefined) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jQuery'));
+    } else {
+        root.Truncate = factory(root.jQuery);
+    }
+}(this, function ($) {
 
   var BLOCK_TAGS = ['table', 'thead', 'tbody', 'tfoot', 'tr', 'col', 'colgroup', 'object', 'embed', 'param', 'ol', 'ul', 'dl', 'blockquote', 'select', 'optgroup', 'option', 'textarea', 'script', 'style'];
 
@@ -514,7 +522,7 @@
      */
     collapse: function (retruncate) {
       this.isExplicitlyCollapsed = true;
-      
+
       if (this.isCollapsed) {
         return;
       }
@@ -543,6 +551,6 @@
     });
   };
 
-  module.Truncate = Truncate;
+  return Truncate;
 
-})(this, jQuery);
+}));
